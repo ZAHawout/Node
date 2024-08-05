566398bbf13b18bece4f98cd44662f26cdc9a4f1
ZAHawout <zachary.a.hawout@gmail.com>
2024-08-04 22:17:08 -0400

    Update Feature Refinement

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function dbSetup(doInsert) {
             quantity INTEGER NOT NULL
         );
     `);
+
     if (doInsert) {
         db.run(`
             INSERT INTO ShoppingList (item, quantity)
@@ -35,9 +36,9 @@ function dbSetup(doInsert) {
 }
 
 function appStartedCallback() {
-console.log('App is listening');
-db = new sqlite3.Database('shopping_list.db');
-dbSetup(false);
+    console.log("App is listening");
+    db = new sqlite3.Database("shopping_list.db");
+    dbSetup(false);
 }
 
 app.listen(port, appStartedCallback);
@@ -45,6 +46,14 @@ app.listen(port, appStartedCallback);
 app.get("/", (req, res) => {
     res.status(200).json({ status: true });
 });
+
+function dbResponseHandler(error, res) {
+    if (error) {
+        res.status(500).json({ error });
+    } else {
+        res.status(200).json({ status: true });
+    }
+}
 
 app.get("/api/list", (req, res) => {
     db.all("SELECT * FROM ShoppingList", (error, rows) => {
@@ -63,38 +72,27 @@ app.post("/api/list/new", (req, res) => {
         VALUES (?, ?);        
     `,
         [req.body.item, req.body.quantity],
-        (error, response) => {
-            if (error) {
-                res.status(500).json({ error });
-            } else {
-                res.status(201).json({ response});
-            }
-        }
+        (error, _) => dbResponseHandler(error, res)
     );
 });
 
-app.delete('/api/list/:id', (req, res) => {
-        db.run(
-            `DELETE FROM ShoppingList
-            WHERE id = ?`
-            [req.params.id],
-            (error, response) => {
-                if (error) {
-                    res.status(500).json({ error });
-                } else {
-                    res.status(200).json({ response });
-                }
-            }
-        )
-    });
+app.delete("/api/list/:id", (req, res) => {
+    db.run(
+        `DELETE FROM ShoppingList
+         WHERE id = ?`,
+        [req.params.id],
+        (error, _) => dbResponseHandler(error, res)
+    );
+});
 
-    app.put("/api/list/:id", (req, res) => {
-        db.run(`
-                UPDATE ShoppingList
-                SET item = ?, quantity = ?
-                WHERE id = ?
-            `,
-            [req.body.item, req.body.quantity, req.params.id],
-            (error, _) => dbResponseHandler(error, res)
-        );
-    });
+app.put("/api/list/:id", (req, res) => {
+    db.run(
+        `
+            UPDATE ShoppingList
+            SET item = ?, quantity = ?
+            WHERE id = ?
+        `,
+        [req.body.item, req.body.quantity, req.params.id],
+        (error, _) => dbResponseHandler(error, res)
+    );
+});
